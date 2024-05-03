9701f63c96a4d3778a73425653f5cb93909fdfe4
Abhinav Saxena <abhinav150601@gmail.com>
2024-05-03 23:22:05 +0530

    NewRole Endpoint

---
+++ src/main/java/com/example/RoleBasedAuth/service/RoleService.java
@@ -0,0 +1,16 @@
+package com.example.RoleBasedAuth.service;
+
+import com.example.RoleBasedAuth.dao.RoleDao;
+import com.example.RoleBasedAuth.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleDao roleDao;
+    public Role createNewRole(Role role){
+        return roleDao.save(role);
+    }
+}
